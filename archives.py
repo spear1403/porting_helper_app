093cf98f13a6f80186079df155785d49d4c1fdf9
spear1403 <spear1403@gmail.com>
2019-05-17 21:03:50 +0200

    Delete archives.py

--- archives.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_archive_files(gameDir):
-    archives_list =[]
-    for filename in os.listdir(gameDir):
-        if filename.lower().endswith('.rpa'):
-            archives_list.append(os.path.join(gameDir, filename))
-    return archives_list
